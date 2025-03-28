01e095d785ee754579cf10214693beae26ce0dd6
Pismak Ivan <firewolfx@yandex.ru>
2025-03-28 08:41:40 -0400

    notices and task 2 added

--- src/connect_services.php
+++ src/connect_services.php
@@ -6,12 +6,10 @@ $database = getenv('MYSQL_DATABASE');
 
 const DEFAULT_QUEUE = 'job_queue';
 
-// Connect to MySQL
 $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("MySQL Connection failed: " . $conn->connect_error);
 }
 
-// Connect to Redis
 $redis = new Redis();
 $redis->connect('redis', 6379);
